87e0c5fc78b40149103330e1910baecc3198afa9
ofjdev <ofj.dev@gmail.com>
2018-02-11 16:53:33 +0100

    (Tutorial From W3Schools) Adding example parameter recognition

--- index_express.js
+++ index_express.js
@@ -1,16 +1,19 @@
 var express = require('express');
 var dt = require('./get_datetime');
+var url = require('url');
 
 var app = express();
 var port = 8080;
 
 var total_sum = 0;
 
+
 app.get('/', function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.write('Hello World \n'+'with Express');
 	res.end();
 });
+
 
 app.get('/sum', function (req, res) {
 	total_sum++;
@@ -19,11 +22,13 @@ app.get('/sum', function (req, res) {
 	res.end();
 });
 
+
 app.get('/get', function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.write('Get to View The Sum: '+total_sum);
 	res.end();
 });
+
 
 app.get('/datetime', function(req, res) { 
 	var datetime = dt.myDateTime();
@@ -33,6 +38,7 @@ app.get('/datetime', function(req, res) {
 	res.end();
 });
 
+
 app.get('/url', function(req, res) { 
 	var URL = req.url;
 
@@ -40,6 +46,31 @@ app.get('/url', function(req, res) {
 	res.write('Printing URL: '+URL);
 	res.end();
 });
+
+
+app.get('/paramsYearMonth', function(req, res) { 
+	var URL = req.url;
+	var parsedUrlTrue = url.parse(URL, true);
+	var parsedUrlFalse = url.parse(URL, false);
+	
+	var query = parsedUrlTrue.query;
+	var year = query.year; // parameter name 'year'
+	var month = query.month; // parameter name 'month'
+
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.write('\nPrinting URL: '+URL);
+	//res.write('\nParsed URL True, Query: '+query); THIS IS NOT POSSIBLE
+	// NOT PARSABLE TO STRING
+
+	res.write('\n(Year, Month) = '+year+' '+month);
+	res.end();
+
+	// Tested with:
+	// http://localhost:8080/paramsYearMonth?year=123123&month=octobeeeeerfest
+	// http://localhost:8080/paramsYearMonth?month=February&year=2018
+});
+
+
 
 app.listen(port);
 
