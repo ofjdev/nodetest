57220ee661a1e06d4cf878b4825885b9b87044ec
ofjdev <ofj.dev@gmail.com>
2018-02-12 00:17:47 +0100

    Simplifying the Test Connection
    Connect() does not fail, it would fail if needed, before on the createConnection(). And it is already treated, so let's simplify the code.

--- db.js
+++ db.js
@@ -7,40 +7,30 @@ var DbHost = "localhost";
 
 global.connectionWorked = false;
 
+var dbParams = {
+	host: DbHost,
+	database: DbName,
+	user: DbUserName,
+	password: DbPassword
+};
+
+var con = mysql.createConnection(dbParams); // con.end();
+console.log("Connected successfully to DB with\n"+formatDB_Info());
+
+
 function formatDB_Info(){
 	return "[User='"+DbUserName+"'; Pass='"+DbPassword+
 		 "'; DB='"+DbName+"'; Host='"+DbHost+"']";
 }
 
+
+
 exports.testConnectionDB = function(){
-	var dbParams = {
-		host: DbHost,
-		database: DbName,
-		user: DbUserName,
-		password: DbPassword
-	};
-
-	var con = mysql.createConnection(dbParams);
+	
 	// NOTE: if fails, probably throws an exception
-	// TODO: Study to reuse connections
+	// TODO: Study reusing connections
 
-	console.log("Connected successfully to DB with\n"+formatDB_Info());
-
-	// assuming that 'connect' will never fail
-	var res = con.connect(function(err) {
-	  	/*if (err) {
-	  		console.log("DB Connection FAILED !! with\n"+formatDB_Info());
-	  		//throw err;
-	  		global.connectionWorked = false;
-	  	} else {
-	  		console.log("Connected successfully to DB with\n"+formatDB_Info());
-	  		global.connectionWorked = true;
-	  	}*/
-	});
-
-	//waitMilliseconds(50);
-
-	con.end();
+	var res = con.connect(); // assuming that 'connect' will never fail
 }
 
 function waitMilliseconds(millis){
