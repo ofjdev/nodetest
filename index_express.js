2be3965d6d0a74a3e1e8809e1461c81db6ecc3c0
ofjdev <ofj.dev@gmail.com>
2018-02-11 16:37:10 +0100

    Adding semicolons + adding 'printURL' service
    Adding semicolons (its not compulsory but I want to use the same politics to all sentences in relation to semicolons.
    
    Adding a new service to print the whole URL (starting on the App, obviously: URL service name and parameters)

--- index_express.js
+++ index_express.js
@@ -1,39 +1,46 @@
-var express = require('express')
-var dt = require('./get_datetime')
+var express = require('express');
+var dt = require('./get_datetime');
 
-var app = express()
-var port = 8080
+var app = express();
+var port = 8080;
 
-var total_sum = 0
+var total_sum = 0;
 
 app.get('/', function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('Hello World \n'+'with Express')
-	res.end()
-})
+	res.write('Hello World \n'+'with Express');
+	res.end();
+});
 
 app.get('/sum', function (req, res) {
-	total_sum++
+	total_sum++;
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('Accepted new increment to the total Sum: '+total_sum)
-	res.end()
-})
+	res.write('Accepted new increment to the total Sum: '+total_sum);
+	res.end();
+});
 
 app.get('/get', function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('Get to View The Sum: '+total_sum)
-	res.end()
-})
+	res.write('Get to View The Sum: '+total_sum);
+	res.end();
+});
 
 app.get('/datetime', function(req, res) { 
-	var datetime = dt.myDateTime()
+	var datetime = dt.myDateTime();
 
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.write('Datetime: '+datetime)
-	res.end()
-})
+	res.write('Datetime: '+datetime);
+	res.end();
+});
 
+app.get('/url', function(req, res) { 
+	var URL = req.url;
 
-app.listen(port)
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.write('Printing URL: '+URL);
+	res.end();
+});
+
+app.listen(port);
 
 console.log('Server running with Express on port '+port+'.');
